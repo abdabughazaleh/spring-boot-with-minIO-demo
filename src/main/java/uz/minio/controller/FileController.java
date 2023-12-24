8ebd89eae6ffb59e02c176871e07d9b9b84e2c39
EMKC0526 <emkc0526@emkanfinance.com.sa>
2023-12-24 14:50:31 +0300

    add get temp url configuration

--- src/main/java/uz/minio/controller/FileController.java
+++ src/main/java/uz/minio/controller/FileController.java
@@ -1,11 +1,9 @@
 package uz.minio.controller;
 
-import io.minio.GetObjectArgs;
-import io.minio.GetPresignedObjectUrlArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import io.minio.errors.MinioException;
 import io.minio.http.Method;
+import io.minio.messages.Bucket;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -15,13 +13,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/app-minio")
@@ -61,7 +63,6 @@ public class FileController {
                             .object(fileName)
                             .build()
             );
-
             HttpHeaders headers = new HttpHeaders();
             headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
 
@@ -84,15 +85,68 @@ public class FileController {
         }
     }
 
+    @GetMapping("/get-buckets")
+    public ResponseEntity<List<String>> getList() {
+        try {
+            List<Bucket> bucketList =
+                    minioClient.listBuckets();
+            for (Bucket bucket : bucketList) {
+                System.out.println(bucket.creationDate() + ", " + bucket.name());
+            }
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(bucketList.stream()
+                    .map(Bucket::name)
+                    .collect(Collectors.toList()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+/*    @GetMapping("/object-is-exist")
+    public ResponseEntity<List<String>> isObjectExist(@PathVariable("objectName") String objectName) {
+        try {
+            List<Bucket> bucketList =
+                    minioClient.getObject().bucket();
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(bucketList.stream()
+                    .map(Bucket::name)
+                    .collect(Collectors.toList()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }*/
+
+    @PostMapping("/add-folder/{folderName}")
+    public ResponseEntity<Void> addFolder(@PathVariable("folderName") String folderName) {
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(bucketName)
+                            .object("spring-boot-3/")
+                            .stream(new ByteArrayInputStream(new byte[] {}), 0, -1)
+                            .build());
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     private String getTempUrl(String bucketName, String objectName) throws IOException, NoSuchAlgorithmException, InvalidKeyException, MinioException {
         Map<String, String> reqParams = new HashMap<>();
-   //     reqParams.put("response-content-type", "application/json");
+        //     reqParams.put("response-content-type", "application/json");
+        StatObjectResponse statObjectResponse = minioClient.statObject(StatObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                .build());
+        System.out.println(statObjectResponse);
         String url = minioClient.getPresignedObjectUrl(
                 GetPresignedObjectUrlArgs.builder()
                         .method(Method.GET)
                         .bucket(bucketName)
                         .object(objectName)
-                        .expiry(2, TimeUnit.HOURS)
+                        .expiry(5, TimeUnit.SECONDS)
                         .extraQueryParams(reqParams)
                         .build());
         System.out.println(url);
